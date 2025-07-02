31ec9e5ef8c4b6e6cc0413c104859d5b3d03fcb9
melissarodriguesdesouza <melissa.rodrigues.souza@escola.pr.gov.br>
2025-07-02 07:55:44 -0300

    Add files via upload

---
+++ Melissa 2B/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Série',
+    'Quais os nomes dos dois irmãos de supernatural?',
+    'Dean e Sam'
+)
+
+criaCartao(
+    'Série',
+    'Qual o nome do anjo de supernatural?',
+    'Castiel'
+)
+
+criaCartao(
+    'Série',
+    'Qual era o nome do demônio que ajudava eles?',
+    'Crowley'
+)
+
+criaCartao(
+    'Série',
+    'Qual era o nome dos pais deles?',
+    'Mary e Jonh'
+)
